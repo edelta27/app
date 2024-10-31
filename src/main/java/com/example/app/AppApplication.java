380f52e8e5f72e6d3584619f49852849a358d5c1
Edyta <edysiaborowska27@gmail.com>
2024-10-31 16:46:07 +0100

    add try catch - exception handling

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,5 +1,7 @@
 package com.example.app;
 
+import feign.FeignException;
+import feign.RetryableException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,12 +24,23 @@ public class AppApplication {
 
 	@EventListener(ApplicationStartedEvent.class)
 	public void makeRequestToShawnMendesEndpoint(){
-		ShawnMendesResponse response = shawnMendesClient.makeSearchRequest("shawnmendes",5);
-		//System.out.println(response);
-		List<ShawnMendesResult> results = response.results();
-		results.forEach(
-				shawnMendesResult -> System.out.println(shawnMendesResult.trackName())
-		);
+        try {
+            ShawnMendesResponse response = shawnMendesClient.makeSearchRequest("shawnmendes", 5);
+            //System.out.println(response);
+            List<ShawnMendesResult> results = response.results();
+            results.forEach(
+                    shawnMendesResult -> System.out.println(shawnMendesResult.trackName())
+            );
+        } catch (FeignException.FeignClientException feignException) {
+            System.out.println("client exception: " + feignException.status());
+        }catch (FeignException.FeignServerException feignException) {
+            System.out.println("server exception: " + feignException.status());
+        }catch (RetryableException retryableException) {
+            System.out.println("retryable exception " + retryableException.getMessage());
+        }catch (FeignException feignException) {
+            System.out.println(feignException.getMessage());
+            System.out.println(feignException.status());
+        };
 
 	}
 
